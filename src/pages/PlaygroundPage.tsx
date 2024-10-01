afd3bec5939a5b671cab27cdf574dac27cbb29d2
FourSqweez <jeerasak.kr@chomchob.com>
2024-10-01 14:30:36 +0700

    Add : test expand and truncate text component with userEvent

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,9 @@
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return (
+    <ExpandableText text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi corrupti ipsum doloribus sed autem unde fugit, ex facilis aut ut veritatis dignissimos reprehenderit provident pariatur aliquam cum deleniti harum quod voluptatum. Voluptates ullam nihil nemo minima esse aspernatur nobis. Ab quo dolorem, ex velit beatae qui perferendis aperiam molestias, commodi consequuntur delectus tempore, quasi expedita ipsum nesciunt eum harum soluta ad accusamus nobis nulla provident. Provident, architecto quam ex consequuntur ad mollitia. Blanditiis explicabo, eaque incidunt numquam nulla sed, praesentium consectetur ipsa ipsam maiores voluptate enim deleniti molestias beatae voluptatibus ut aliquam cum laboriosam laudantium ad qui alias! Similique." />
+  );
 };
 
 export default PlaygroundPage;
