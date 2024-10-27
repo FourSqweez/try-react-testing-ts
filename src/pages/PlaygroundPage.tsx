a1e27ba9204192a2778213f594d697f0e17654bf
Foursqweez <jeerasak.code@gmail.com>
2024-10-27 16:33:35 +0700

    Testing SearchBox with input and enter action and onChange to with arg

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,7 @@
-import ExpandableText from "../components/ExpandableText";
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
-  return (
-    <ExpandableText text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. A commodi corrupti ipsum doloribus sed autem unde fugit, ex facilis aut ut veritatis dignissimos reprehenderit provident pariatur aliquam cum deleniti harum quod voluptatum. Voluptates ullam nihil nemo minima esse aspernatur nobis. Ab quo dolorem, ex velit beatae qui perferendis aperiam molestias, commodi consequuntur delectus tempore, quasi expedita ipsum nesciunt eum harum soluta ad accusamus nobis nulla provident. Provident, architecto quam ex consequuntur ad mollitia. Blanditiis explicabo, eaque incidunt numquam nulla sed, praesentium consectetur ipsa ipsam maiores voluptate enim deleniti molestias beatae voluptatibus ut aliquam cum laboriosam laudantium ad qui alias! Similique." />
-  );
+  return <SearchBox onChange={(text) => console.log(text)} />;
 };
 
 export default PlaygroundPage;
